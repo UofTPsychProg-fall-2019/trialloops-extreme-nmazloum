442236e446bc2706430907e1fbe66f0c7610a8df
Negar Mazloum-Farzaghi <55118623+nmazloum@users.noreply.github.com>
2019-12-03 13:53:02 -0500

    FINAL SUBMISSION-NEGAR MAZLOUM-FARZAGHI
    FINAL SUBMISSION-NEGAR MAZLOUM-FARZAGHI

--- negar.py
+++ negar.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-This experiment was created using PsychoPy3 Experiment Builder (v3.2.4),
-    on Tue Dec  3 12:53:54 2019
-If you publish work using this script the most relevant publication is:
 
-    Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
-        PsychoPy2: Experiments in behavior made easy Behav Res 51: 195. 
-        https://doi.org/10.3758/s13428-018-01193-y
-
-"""
 
 from __future__ import absolute_import, division
 
